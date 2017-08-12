ebd84df3e43d3c45361b31989e70a222ef8b8593
Dominique VASSARD <dominique.vassard@gmail.com>
2017-08-12 17:04:25 +0200

    Fix vinculi_api test (vinculi_graph dependency check)

--- apps/vinculi_api/test/vinculi_api/deps/vinculi_graph_test.exs
+++ apps/vinculi_api/test/vinculi_api/deps/vinculi_graph_test.exs
@@ -4,7 +4,19 @@ defmodule VinculiApi.Deps.VinculiGraphTest do
   alias VinculiGraph.TestPerson
   alias VinculiGraph.Repo
 
+  @valid_changes %{
+    firstName: "Test_firstName",
+    lastName: "Test_lastname",
+    uuid: Ecto.UUID.generate()
+  }
+
   test "[get!] single node based on uuid" do
+    changes = Map.put(@valid_changes, :uuid, "TestPerson-1")
+    {res, _} = %TestPerson{}
+      |> TestPerson.changeset(changes)
+      |> Repo.insert()
+      assert res == :ok
+
     Repo.get!(TestPerson, "TestPerson-1")
   end
 end
