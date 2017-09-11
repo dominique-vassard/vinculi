2a88eba5c711b95ee7b7f8d006ab17ca7ec1c6b2
Dominique VASSARD <dominique.vassard@gmail.com>
2017-09-11 15:23:34 +0200

    [VINCULI_WEB] Test session controller/view

---
+++ apps/vinculi_web/test/vinculi_web/views/coherence/session_view_test.exs
@@ -0,0 +1,21 @@
+defmodule VinculiWeb.Coherence.SessionViewtest do
+  use VinculiWeb.ConnCase, async: true
+  import Phoenix.View
+
+  alias VinculiDb.Coherence.Schemas
+
+  describe "Test with locale" do
+    setup [:setup_locale]
+
+    @tag locale: "fr"
+    test "GET /invitations/new", %{conn: conn} do
+
+      content = render_to_string(VinculiWeb.Coherence.SessionView,
+                                 "new.html", conn: conn, remember: false)
+      assert String.contains? content, "Email"
+      assert String.contains? content, "Mot de passe"
+      assert String.contains? content, "Mot de passe oublié?"
+      assert String.contains? content, "S&#39;inscrire"
+    end
+  end
+end
