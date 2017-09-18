56658f3859b73cb3f478d14b477464b9a8985d23
Dominique VASSARD <dominique.vassard@gmail.com>
2017-09-18 14:49:20 +0200

    [VINCULI_WEB] Menu and layout design

--- apps/vinculi_web/lib/vinculi_web/views/layout_view.ex
+++ apps/vinculi_web/lib/vinculi_web/views/layout_view.ex
@@ -1,4 +1,12 @@
 defmodule VinculiWeb.LayoutView do
   use VinculiWeb, :view
   import VinculiWeb.Coherence.ViewHelpers
+
+  def account_menu(%{assigns: %{current_user: nil}} = conn) do
+    render(__MODULE__, "menu_no_user.html", conn: conn)
+  end
+
+  def account_menu(%{assigns: %{current_user: _user}} = conn) do
+    render(__MODULE__, "menu_user.html", conn: conn)
+  end
 end
