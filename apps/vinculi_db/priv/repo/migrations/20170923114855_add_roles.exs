f6f9ef2b5584a9078ce1d80be5377eb046761332
Dominique VASSARD <dominique.vassard@gmail.com>
2017-09-24 19:39:25 +0200

    [VINCULI_DB] Review database to add non null and indexes where needed

--- apps/vinculi_db/priv/repo/migrations/20170923114855_add_roles.exs
+++ apps/vinculi_db/priv/repo/migrations/20170923114855_add_roles.exs
@@ -4,32 +4,41 @@ defmodule VinculiDb.Repo.Migrations.AddRoles do
   def change do
     # Permissions table
     create table(:permissions) do
-      add :name, :string
+      add :name, :string, null: false
 
       timestamps()
     end
+
+    # Permission name should be unique
+    create unique_index(:permissions, :name)
 
     # Roles table
     create table(:roles) do
-      add :name, :string
+      add :name, :string, null: false
 
       timestamps()
     end
+
+    # Role name should be unique
+    create unique_index(:roles, :name)
 
     # Permission to role table
-    create table(:role_permissions) do
+    create table(:role_permissions, primary_key: false) do
       add :role_id, references(:roles, on_delete: :delete_all,
-                               on_update: :update_all)
+                               on_update: :update_all), null: false
       add :permission_id, references(:permissions, on_delete: :delete_all,
-                                     on_update: :update_all)
+                                     on_update: :update_all), null: false
 
       timestamps()
     end
+
+    # Role <-> permission association should be unique
+    create unique_index(:role_permissions, [:role_id, :permission_id])
 
     # Add role to users table
     alter table(:users) do
       add :role_id, references(:roles, on_delete: :nilify_all,
-                               on_update: :update_all)
+                               on_update: :update_all), null: false
     end
   end
 end
