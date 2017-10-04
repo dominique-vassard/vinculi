3efb4377a618f70ea4137d5bc8a5e26554abdcf2
Dominique VASSARD <dominique.vassard@gmail.com>
2017-10-04 21:54:11 +0200

    [VINCULI_GRAPH] Add docs

--- apps/vinculi_graph/lib/vinculi_graph/helpers.ex
+++ apps/vinculi_graph/lib/vinculi_graph/helpers.ex
@@ -1,4 +1,15 @@
 defmodule VinculiGraph.Helpers do
+
+  @doc """
+    Retrieve non-id fields defined for schema.
+    Non-id fields are tohse defined as primary key.
+
+    Schemaa  must exists!
+
+    ## Example:
+    iex> get_non_id_fields "Person"
+    [:first_name, :last_name, :aka, :internal_link, :external_link]
+  """
   def get_non_id_fields(schema) do
     module = Module.concat(["VinculiGraph", schema])
 
