33caf8afec5a62b7dcb49cc435ed5a0dca8bdec5
Dominique VASSARD <dominique.vassard@gmail.com>
2017-10-09 18:20:06 +0200

    Remove vinculi_api

--- apps/vinculi_api/lib/vinculi_api/controllers/meta_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule VinculiApi.MetaController do
-  use VinculiApi, :controller
-  alias VinculiGraph.Meta
-
-  def labels(conn, _params) do
-    json conn, Meta.list_labels()
-  end
-end
--- apps/vinculi_api/lib/vinculi_api/controllers/test_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule VinculiApi.TestController do
-  use VinculiApi, :controller
-
-  def index(conn, _params) do
-    json conn, %{test: "ok"}
-  end
-end
