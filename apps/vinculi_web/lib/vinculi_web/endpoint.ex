02fe274e34ab81d216ee43fe9536e801edafd245
Dominique VASSARD <dominique.vassard@gmail.com>
2017-11-05 13:46:26 +0100

    [VINCULI_WEB] Manage graph static files and unused static files

--- apps/vinculi_web/lib/vinculi_web/endpoint.ex
+++ apps/vinculi_web/lib/vinculi_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule VinculiWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :vinculi_web, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js graph_style favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
