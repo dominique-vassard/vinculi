901ca3b8bd31bde874ce9898018f6f56e1154904
Dominique VASSARD <dominique.vassard@gmail.com>
2017-11-26 20:31:03 +0100

    [VINCULI_WEB] Add environment to config

--- apps/vinculi_web/config/test.exs
+++ apps/vinculi_web/config/test.exs
@@ -3,6 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :vinculi_web, VinculiWeb.Endpoint,
+  env: :test,
   http: [port: 4001],
   server: false
 
