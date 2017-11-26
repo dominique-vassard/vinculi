11e89f4194eab3c0dea5e11526c6d5d827852064
Dominique VASSARD <dominique.vassard@gmail.com>
2017-11-26 21:55:41 +0100

    Fix environment configuration

--- apps/vinculi_web/config/test.exs
+++ apps/vinculi_web/config/test.exs
@@ -3,9 +3,11 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :vinculi_web, VinculiWeb.Endpoint,
-  env: :test,
   http: [port: 4001],
   server: false
+
+# Environement information
+config :vinculi_web, env: :test
 
 # Basic auth configs
 config :vinculi_web,
